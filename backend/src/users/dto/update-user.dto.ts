70b9e23ef79d06e3d7d1fbb5f72b998f88cee44d
Yansmtp <yans.turro@gmail.com>
2026-03-28 13:10:53 -0400

    feat: add users module and admin guard

---
+++ backend/src/users/dto/update-user.dto.ts
@@ -0,0 +1,22 @@
+import { IsEmail, IsIn, IsOptional, IsString, MinLength } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  @MinLength(3)
+  name?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(6)
+  password?: string;
+
+  @IsOptional()
+  @IsString()
+  @IsIn(['ADMIN', 'USER'])
+  role?: string;
+}
